ba60c37f8421da2b6f6745b1a6fb24d5f0039aeb
Bill Farmer <williamjfarmer@yahoo.co.uk>
2017-01-23 19:13:06 +0000

    remove native sources

--- jni/Application.mk
+++
@@ -1,6 +0,0 @@
-# Uncomment the following line to build all android architectures, or
-# change to architectures required (armeabi armeabi-v7a x86 mips). You
-# will also need to add the relevent copies of libsonivox.so to the
-# NDK.
-
-APP_ABI := all
