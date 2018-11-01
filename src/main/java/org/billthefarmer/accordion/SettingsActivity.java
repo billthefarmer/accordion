cf1f418eea55b931dca79536a60faa8148e84971
TacoTheDank <SkytkRSfan3895@gmail.com>
2018-11-01 10:58:15 -0400

    src
    Signed-off-by: TacoTheDank <SkytkRSfan3895@gmail.com>

--- src/main/java/org/billthefarmer/accordion/SettingsActivity.java
+++ src/main/java/org/billthefarmer/accordion/SettingsActivity.java
@@ -29,38 +29,34 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 // SettingsActivity
-public class SettingsActivity extends Activity
-{
+public class SettingsActivity extends Activity {
     // On create
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-	super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	// Display the fragment as the main content.
-	getFragmentManager().beginTransaction()
-	    .replace(android.R.id.content, new SettingsFragment())
-	    .commit();
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
 
-	// Enable back navigation on action bar
-	ActionBar actionBar = getActionBar();
-	actionBar.setDisplayHomeAsUpEnabled(true);
+        // Enable back navigation on action bar
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     // On options item selected
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-	// Switch on item id
-	switch (item.getItemId())
-	{
-	case android.R.id.home:
-	    // app icon in action bar clicked; go home
-            finish();
-	    return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Switch on item id
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                finish();
+                return true;
 
-	default:
-	    return false;
-	}
+            default:
+                return false;
+        }
     }
 }
